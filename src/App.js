3921905c28f44237758f7d9d285d3d0147680ad9
lynn1481 <61997876+lynn1481@users.noreply.github.com>
2023-07-30 03:07:16 -0400

    React Navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
+
+import React from 'react';
+import Navbar from "./components/Navbar";
+
+
+const App = () => {
+    return (
+        <React.Fragment>
+            <Navbar/>
+        </React.Fragment>
+    );
+  }
+  
+  export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "../styles/main.css";
+
+function Navbar() {
+    const navRef = useRef();
+    
+    const showNavbar = () => {
+        navRef.current.classList.toggle("responsive_nav");
+    };
+
+    return(
+        <header>
+            <h3>Momentum</h3>
+            <nav ref={navRef}>
+                <a href="/#">Tracker</a>
+                <a href="/#">Music</a>
+                <a href="/#">Login</a>
+                <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+                    <FaTimes/>
+                </button>
+            </nav>
+            <button className="nav-btn" onClick = {showNavbar}>
+                    <FaBars/>
+            </button>
+        </header>
+    );
+}
+ 
+ export default Navbar;
+ 

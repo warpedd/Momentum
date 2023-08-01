93701fb1c866bb258bee4f33f34cc299938ff2ab
RJMaradiaga <ryanjosue.rj@gmail.com>
2023-08-01 17:09:42 -0400

    deleted files because of api directory was renamed apiv1, and added package express-rate-limit for api limiter to prevent abuse

--- backend/api/routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-let express = require('express');
-let router = express.Router();
-let UserController = require('../controllers/userController.js');
-
-router.get('/', UserController.createUser);
-
-module.exports = router; 

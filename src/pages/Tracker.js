b6b1e2af5587a9d6d6550700523a23e81e95bd48
Abdul <abdelraheem_jaara@student.uml.edu>
2023-08-01 13:17:17 -0400

    Track is a work in progress

--- src/pages/Tracker.js
+++ src/pages/Tracker.js
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, { useState } from "react";
 
-function Tracker() {
-  return (
-    <div className='Tracker'>
-      <h1>Tracker</h1>
-    </div>
-  );
-}
+const Tracker = () => {
+	const [task, setTask] = useState("");
+	const [tasks, setTasks] = useState([]);
+	const [completedTasks, setCompletedTasks] = useState([]);
+
+	const handleInputChange = (event) => {
+		setTask(event.target.value);
+	};
+
+	const handleAddTask = () => {
+		if (task !== "") {
+			setTasks([...tasks, task]);
+			setTask("");
+		}
+	};
+
+	const handleCompleteTask = (index) => {
+		const newTasks = [...tasks];
+		const completedTask = newTasks.splice(index, 1);
+		setTasks(newTasks);
+		setCompletedTasks([...completedTasks, ...completedTask]);
+	};
+
+	return (
+		<div className="Tracker">
+			<h1>Tracker</h1>
+			<input
+				type="text"
+				value={task}
+				onChange={handleInputChange}
+				placeholder="Task Name"
+			/>
+			<button onClick={handleAddTask}>Add Task</button>
+			<h2>Remaining</h2>
+			<ul>
+				{tasks.map((task, index) => (
+					<li key={index}>
+						{task}
+						<input type="checkbox" onChange={() => handleCompleteTask(index)} />
+					</li>
+				))}
+			</ul>
+			<h2>Completed</h2>
+			<ul>
+				{completedTasks.map((task, index) => (
+					<li key={index}>{task}</li>
+				))}
+			</ul>
+		</div>
+	);
+};
 
 export default Tracker;

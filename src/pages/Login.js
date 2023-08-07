4e85cc294cc2399832e6a924045cbd95bcf16368
lynn1481 <61997876+lynn1481@users.noreply.github.com>
2023-08-06 22:04:26 -0400

    forgot password function

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { useHistory } from 'react-router-dom';
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { Link, useHistory } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword, sendPasswordResetEmail } from 'firebase/auth';
 import "../styles/Login.css";
 
 export const Login = (props) => {
@@ -22,28 +22,36 @@ export const Login = (props) => {
             setError(error.message);
         }
     }
-  
+
+    const forgotPassword = () => {
+        sendPasswordResetEmail(getAuth(), email)
+
+        .then(() => { alert("Password reset email sent!") })
+        .catch((error) => { alert(error) })
+    }
+
     return (
         <div className="login-box">
             <h2>Login</h2>
             {error && <p className="error">{error}</p>}
             <form className="login-form" onSubmit={handleLogIn}>
                 <label htmlFor="email">Email</label>
-                <input 
-                    value={email} 
+                <input
+                    value={email}
                     onChange={(e) => setEmail(e.target.value)}
-                    type="email" 
-                    placeholder="Enter email" 
-                    id="email" 
+                    type="email"
+                    placeholder="Enter email"
+                    id="email"
                     name="email" />
                 <label htmlFor="password">Password</label>
-                <input 
-                    value={pass} 
-                    onChange={(e) => setPass(e.target.value)} 
-                    type="password" 
-                    placeholder="Enter password" 
-                    id="password" 
+                <input
+                    value={pass}
+                    onChange={(e) => setPass(e.target.value)}
+                    type="password"
+                    placeholder="Enter password"
+                    id="password"
                     name="password" />
+                <button className="forgot-button-generic" onClick={() => forgotPassword()}>Forgot Password</button>
                 <button className="login-button-generic" type="submit">Log In</button>
             </form>
             <button className="login-link-button-generic" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
